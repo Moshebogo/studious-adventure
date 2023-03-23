890ff7d297a186665bc5fd79f916009f1e9487b5
Eli Moshe <moshebogo@gmail.com>
2023-03-23 08:53:17 -0400

    cc complete!

--- server/app.py
+++ server/app.py
@@ -17,26 +17,81 @@ db.init_app(app)
 @app.route('/')
 def home():
     return ''
-
+#  route for all HEROES
 @app.route('/heroes')
 def heroes():
-    return ''
+    heroes = Hero.query.all()
+    heroes_dict = [hero.to_dict() for hero in heroes]
+    return make_response(jsonify(heroes_dict), 200)
 
-@app.route('/heroes/<int:id>')
+#  route for HERO by ID
+@app.route('/heroes/<int:id>', methods = ['GET'])
 def hero_by_id(id):
-    return ''
-
-@app.route('/powers')
+    hero_exists = Hero.query.get(id)
+    # validates if hero exists or not
+    if not hero_exists:
+        return make_response(jsonify({"error": "Hero not found"}), 404)
+    # if hero_exists is True:
+    if hero_exists:
+        if request.method == 'GET':
+            return make_response(jsonify(hero_exists.to_dict()), 200)
+    
+# route for all POWERS
+@app.route('/powers', methods = ['GET'])
 def powers():
-    return ''
+    if request.method == 'GET':
+        powers = Power.query.all()
+        powers_dict = [power.to_dict() for power in powers]
+        return make_response(jsonify(powers_dict), 200)
 
-@app.route('/powers/<int:id>')
+# route for POWER by ID
+@app.route('/powers/<int:id>', methods = ['GET', 'PATCH'])
 def power_by_id(id):
-    return ''
+    power_exists = Power.query.get(id)
+    # validate if power exists or not
+    if not power_exists:
+        return make_response(jsonify({ "error": "Power not found"}), 404)
+    # if power_exists is True:
+    if power_exists:
+        # GET for POWER by ID
+        if request.method == 'GET':
+            return make_response(jsonify(power_exists.to_dict()), 200)
+        # PATCH for POWER by ID
+        if request.method == 'PATCH':
+            body = request.get_json()
+            for key, value in body.items():
+                setattr(power_exists, key, value)   
+            db.session.add(power_exists)
+            db.session.commit()
+            return make_response(jsonify(power_exists.to_dict()), 200)
 
-@app.route('/hero_powers')
+# route for all HeroPower
+@app.route('/hero_powers', methods = ['GET', 'POST'])
 def hero_powers():
-    return ''
+    # GET for all HeroPower (the lazy sort way)
+    if request.method == 'GET':
+        return make_response(jsonify([hp.to_dict() for hp in HeroPower.query.all()]), 200)
+    # POST for HeroPower
+    if request.method == 'POST':
+        body = request.get_json()
+        new_hero_power = HeroPower()
+        for key, value in body.items():
+            setattr(new_hero_power, key, value)
+        #  validates hero, power and strength all exist, if not, returns correct error (also the lazy way)
+        hero_exists = Hero.query.get(new_hero_power.hero_id) 
+        power_exists = Power.query.get(new_hero_power.power_id) 
+        strengths = ['Strong', 'Average', 'Weak'] 
+        if not hero_exists:
+            return {"error": "hero does not exist"}, 422 
+        elif not power_exists:
+            return {"error": "power does not exist"}, 422
+        elif new_hero_power.strength not in strengths:
+            return {"error": "invalid strength"}, 422
+        # and if all both exist, creates it
+        elif hero_exists and power_exists and new_hero_power.strength in strengths:
+            db.session.add(new_hero_power)
+            db.session.commit()
+            return make_response(jsonify(hero_exists.to_dict()), 201)    
 
 
 if __name__ == '__main__':
