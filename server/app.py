d6acc685eef43a51fd9bbd22c28d4a954d6a465a
Eli Moshe <moshebogo@gmail.com>
2023-03-23 09:37:31 -0400

    all GET POST PATCH DELETE

--- server/app.py
+++ server/app.py
@@ -18,14 +18,25 @@ db.init_app(app)
 def home():
     return ''
 #  route for all HEROES
-@app.route('/heroes')
+@app.route('/heroes', methods = ['GET' ,'POST'])
 def heroes():
-    heroes = Hero.query.all()
-    heroes_dict = [hero.to_dict() for hero in heroes]
-    return make_response(jsonify(heroes_dict), 200)
+    # GET for HERO by ID
+    if request.method == 'GET':
+        heroes = Hero.query.all()
+        heroes_dict = [hero.to_dict() for hero in heroes]
+        return make_response(jsonify(heroes_dict), 200)
+    # POST for HERO by ID
+    elif request.method == 'POST':
+        body = request.get_json()
+        new_hero = Hero()
+        for key, value in body.items():
+            setattr(new_hero, key, value)
+        db.session.add(new_hero)   
+        db.session.commit() 
+        return make_response(jsonify(new_hero.to_dict()), 201)
 
 #  route for HERO by ID
-@app.route('/heroes/<int:id>', methods = ['GET'])
+@app.route('/heroes/<int:id>', methods = ['GET', 'DELETE', 'PATCH'])
 def hero_by_id(id):
     hero_exists = Hero.query.get(id)
     # validates if hero exists or not
@@ -33,19 +44,43 @@ def hero_by_id(id):
         return make_response(jsonify({"error": "Hero not found"}), 404)
     # if hero_exists is True:
     if hero_exists:
+        # GET for HERO by ID 
         if request.method == 'GET':
             return make_response(jsonify(hero_exists.to_dict()), 200)
+        # DELETE for HERO by ID
+        elif request.method == 'DELETE':
+            db.session.delete(hero_exists)
+            db.session.commit()
+            return make_response(jsonify({"status": "DELETE successful"}), 200)
+        # PATCH for HERO by ID
+        elif request.method == 'PATCH':
+            body = request.get_json()
+            for key, value in body.items():
+                setattr(hero_exists, key, value)
+            db.session.add(hero_exists)
+            db.session.commit()
+            return make_response(jsonify(hero_exists.to_dict()), 200)    
     
 # route for all POWERS
-@app.route('/powers', methods = ['GET'])
+@app.route('/powers', methods = ['GET', 'POST'])
 def powers():
+    # GET for all POWERS
     if request.method == 'GET':
         powers = Power.query.all()
         powers_dict = [power.to_dict() for power in powers]
         return make_response(jsonify(powers_dict), 200)
+    # POST for all POWERS
+    elif request.method == 'POST':
+        body = request.get_json()
+        new_power = Power()
+        for key, value in body.items():
+            setattr(new_power, key, value)
+        db.session.add(new_power)
+        db.session.commit()
+        return make_response(jsonify(new_power.to_dict()), 201)    
 
 # route for POWER by ID
-@app.route('/powers/<int:id>', methods = ['GET', 'PATCH'])
+@app.route('/powers/<int:id>', methods = ['GET', 'DELETE', 'PATCH'])
 def power_by_id(id):
     power_exists = Power.query.get(id)
     # validate if power exists or not
@@ -56,6 +91,11 @@ def power_by_id(id):
         # GET for POWER by ID
         if request.method == 'GET':
             return make_response(jsonify(power_exists.to_dict()), 200)
+        # DELETE for POWERS by ID
+        elif request.method == 'DELETE':
+            db.session.delete(power_exists)
+            db.session.commit()
+            return make_response(jsonify({"status": "DELETE successful"}), 200)
         # PATCH for POWER by ID
         if request.method == 'PATCH':
             body = request.get_json()
